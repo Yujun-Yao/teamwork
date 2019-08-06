3e2142904069aeacaf8df740469459eefeedc4f0
Yujun-Yao <yaoyujun06@163.com>
2019-08-06 20:08:51 +0800

    2019/8/6作业

---
+++ StudentOperatable.java
@@ -0,0 +1,33 @@
+package com.train.unit19;
+
+import java.util.List;
+
+/**
+ *
+ * @Author 姚玉军
+ * @Date 2019/8/6
+ * @Version V1.0
+ */
+public interface StudentOperatable {
+    /**
+     * 根据姓名模糊查找学生
+     * @param stuName 姓名
+     * @return 学生集合
+     */
+    List<Student> queryStudentByNameLike(String stuName);
+
+    /**
+     * 查询所有学生并根据学生姓名降序排列
+     * @return 学生集合
+     */
+    List<Student> queryStudentOrderByName();
+
+    /**
+     * 根据姓名模糊查找学生并进行升序排列，并进行分页展示
+     * @param stuName 学生姓名
+     * @param curPage 当前页数
+     * @param pageSize 一页显示几条数据
+     * @return 学生集合
+     */
+    List<Student> queryStudentByNameLikeOrderLimit(String stuName, int curPage, int pageSize);
+}
